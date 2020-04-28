0dfcdab9bafc2037562bbfaa1454be48e3d206d1
mato <mato@splitmedialabs.com>
2020-04-28 20:47:55 +0800

    portfolio inital update

---
+++ precache-manifest.e5c6387961ea66155952aa1a9975bb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d087031f592681929a434f074cce3c81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598bcf9b513d482aa51f",
+    "url": "/static/css/main.18c9229f.chunk.css"
+  },
+  {
+    "revision": "c644d0ca25e2503e09e7",
+    "url": "/static/js/2.0da940d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0da940d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598bcf9b513d482aa51f",
+    "url": "/static/js/main.0231e7a1.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  }
+]);
